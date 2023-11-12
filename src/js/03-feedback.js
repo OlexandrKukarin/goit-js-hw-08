3f08e6fb0ac0c69b23a9dde304a2531946dc296b
OlexandrKukarin <19kukarin99@gmail.com>
2023-11-12 19:20:42 +0200

    q

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,11 +1,13 @@
 import throttle from 'lodash.throttle';
 
 const form = document.querySelector('.feedback-form');
-const inputEmail = document.querySelector('input[name="email"]');
-const textArea = document.querySelector('textarea[name="message"]');
-const btn = document.querySelector('button');
+// const inputEmail = document.querySelector('input[name="email"]');
+// const textArea = document.querySelector('textarea[name="message"]');
+// const btn = document.querySelector('button');
 
 const LOCAL_KEY = 'feedback-form-state';
+
+let formData = {};
 
 form.addEventListener('submit', handleSub);
 form.addEventListener('input', throttle(handleSave, 500));
@@ -15,27 +17,36 @@ function handleSub(e) {
   localStorage.removeItem(LOCAL_KEY);
   // inputEmail.value = '';
   // textArea.value = '';
-  console.log({
-    email: inputEmail.value,
-    message: textArea.value,
-  });
-  form.reset();
+  console.log(formData);
+  formData = {};
+  // console.log({
+  //   email: inputEmail.value,
+  //   message: textArea.value,
+  // });
+  e.target.reset();
 }
 
 function handleSave(e) {
-  const formSave = {
-    email: inputEmail.value,
-    message: textArea.value,
-  };
-  localStorage.setItem(LOCAL_KEY, JSON.stringify(formSave));
+  // const formSave = {
+  //   email: inputEmail.value,
+  //   message: textArea.value,
+  // };
+  const inputValue = e.target.value.trim();
+  const inputName = e.target.name;
+  formData[inputName] = inputValue;
+  localStorage.setItem(LOCAL_KEY, JSON.stringify(formData));
 }
 
-// function loadForm() {
-//   const storForm = localStorage.getItem(LOCAL_KEY);
-//   if (storForm) {
-//     const formS = JSON.parse(storForm);
-//     inputEmail = formS.email;
-//     textArea = formS.message;
-//   }
-// }
-// loadForm();
+refreshForm();
+function refreshForm() {
+  try {
+    const saveData = localStorage.getItem(LOCAL_KEY);
+    if (!saveData) return;
+    formData = JSON.parse(saveData);
+    Object.entries(formData).forEach(([key, val]) => {
+      form.elements[key].value = val;
+    });
+  } catch ({ message }) {
+    console.log(message);
+  }
+}
